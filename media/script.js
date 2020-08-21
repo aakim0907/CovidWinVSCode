dd408d5bb9025a2d478b3210d6a26dbdde5a1db9
Young Eun Kim <kimy@microsoft.com>
2020-08-21 12:34:06 -0700

    connect api call and render site names

--- media/script.js
+++ media/script.js
@@ -1,13 +1,58 @@
 (function () {
+    const vscode = acquireVsCodeApi();
+
     let count = 0;
     const counter = document.getElementById('lines-of-code-counter');
-    const input = document.getElementById("state-name");
+    const inputDiv = document.getElementById("state-name");
+    const testingSitesDiv = document.getElementById('testing-sites');
 
-    input.addEventListener("keyup", (e)=> {
+    class CovidTestingSites {
+        constructor(testingSitesDiv) {
+            this.parentDiv = testingSitesDiv;
+        }
+
+        _fetchSites() {
+            const h1 = document.createElement('h1');
+            h1.textContent = 'hello';
+            this.parentDiv.appendChild(h1);
+
+            try {
+                fetch("https://covid-19-testing.github.io/locations/washington/complete.json")
+                .then(response => response.json())
+                .then(data => {
+                    const h1 = document.createElement('h1');
+                    h1.textContent = 'hi';
+                    this.parentDiv.appendChild(h1);
+
+                    data.forEach(d => {
+                        const h2 = document.createElement('h2');
+                        h2.textContent = d.name;
+                        this.parentDiv.appendChild(h2);
+                    });
+                });
+            } catch (error) {
+                vscode.postMessage({
+                    command: 'alert',
+                    text: error.message
+                });
+            }
+        }
+    }
+
+    const testingSites = new CovidTestingSites(inputDiv, testingSitesDiv);
+
+    inputDiv.addEventListener("keyup", (e) => {
         if (e.keyCode === 13) {
             e.preventDefault();
-
             counter.textContent = count++;
+            try {
+                testingSites._fetchSites();
+            } catch (error) {
+                vscode.postMessage({
+                    command: 'alert',
+                    text: error.message
+                });
+            }
         }
     });
 }());
---
+++ src/provider.ts
@@ -0,0 +1,94 @@
+import * as path from 'path';
+import * as vscode from 'vscode';
+
+function getNonce() {
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+}
+
+export class CovidProvider implements vscode.CustomEditorProvider<any> {
+
+    public static register(context: vscode.ExtensionContext): vscode.Disposable {
+		return vscode.commands.registerCommand('covidvscode.start', () => {
+            const panel = vscode.window.createWebviewPanel(
+                'catCoding',
+                'COVID Testing Sites',
+                vscode.ViewColumn.One,
+                {
+                    enableScripts: true,
+                } // Webview options.
+            );
+            // And set its HTML content
+            panel.webview.html = this.getWebviewContent(context.extensionUri, panel.webview);
+
+            panel.webview.onDidReceiveMessage(
+                message => {
+                  switch (message.command) {
+                    case 'alert':
+                      vscode.window.showErrorMessage(message.text);
+                      return;
+                  }
+                },
+                undefined,
+                context.subscriptions
+              );
+        });
+    }
+    
+    static getWebviewContent = (extensionUri: vscode.Uri, webview: vscode.Webview) => {
+        const nonce = getNonce();
+
+        const stylesheetPathOnDisk = vscode.Uri.joinPath(extensionUri, 'media', 'style.css');
+        const stylesheetUri = webview.asWebviewUri(stylesheetPathOnDisk);
+    
+        const scriptPathOnDisk = vscode.Uri.joinPath(extensionUri, 'media', 'script.js');
+        const scriptUri = webview.asWebviewUri(scriptPathOnDisk);
+    
+        return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>COVID Testing Sites</title>
+        <link rel="stylesheet" type="text/css" href ="${stylesheetUri}">
+    </head>
+    <body>
+        <div class="main">
+            <h3>Check COVID Testing Sites Near You</h3>
+            <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="250" />
+            <h1 id="lines-of-code-counter">0</h1>
+    
+            <input type="text" id="state-name" />
+            <div id="testing-sites"></div>
+        <div/>
+        
+        <script nonce="${nonce}" src="${scriptUri}"></script>
+    </body>
+    </html>`;
+    };
+
+    private readonly _onDidChangeCustomDocument = new vscode.EventEmitter<vscode.CustomDocumentEditEvent<any>>();
+	public readonly onDidChangeCustomDocument = this._onDidChangeCustomDocument.event;
+    saveCustomDocument(document: any, cancellation: vscode.CancellationToken): Thenable<void> {
+        throw new Error("Method not implemented.");
+    }
+    saveCustomDocumentAs(document: any, destination: vscode.Uri, cancellation: vscode.CancellationToken): Thenable<void> {
+        throw new Error("Method not implemented.");
+    }
+    revertCustomDocument(document: any, cancellation: vscode.CancellationToken): Thenable<void> {
+        throw new Error("Method not implemented.");
+    }
+    backupCustomDocument(document: any, context: vscode.CustomDocumentBackupContext, cancellation: vscode.CancellationToken): Thenable<vscode.CustomDocumentBackup> {
+        throw new Error("Method not implemented.");
+    }
+    openCustomDocument(uri: vscode.Uri, openContext: vscode.CustomDocumentOpenContext, token: vscode.CancellationToken) {
+        throw new Error("Method not implemented.");
+    }
+    resolveCustomEditor(document: any, webviewPanel: vscode.WebviewPanel, token: vscode.CancellationToken): void | Thenable<void> {
+        throw new Error("Method not implemented.");
+    }
+}
